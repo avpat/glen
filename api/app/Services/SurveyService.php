c4fff332373a32bbe7bbbc5f5809f66c388515ee
avi <abhi.uk96@gmail.com>
2020-07-01 18:13:37 +0100

    [task-1] create basic survey service

---
+++ api/app/Services/SurveyService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+class SurveyService
+{
+    //it takes payLoad as a collection
+    //it takes eligibility survey (collection of rules)
+    //evaluates and returns a boolean response of the evalution
+    public function handlePayload()
+    {
+
+    }
+
+    private function evaluate()
+    {
+
+
+    }
+}
