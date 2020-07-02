d3e6d00cd48d0ac8abaf9fad2d0fdb790cce151a
avi <abhi.uk96@gmail.com>
2020-07-02 15:16:30 +0100

    [additional] calling survey service interface from the homepage

---
+++ api/app/Http/Controllers/SurveyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rule;
+use App\Services\SurveyService;
+use App\Survey;
+
+
+
+use Illuminate\Http\Request;
+
+class SurveyController extends Controller
+{
+
+    /**
+     * Only for the test purpose pass the data via controller
+     * @param Request $request
+     * @return array
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     *
+     */
+    public function index(Request $request)
+    {
+        //number 6 record has rules
+        $survey = Survey::find( 6);
+        //create a sample payload
+        $payLoad = collect([
+            'Annual Income'             => 30000,
+            'Borrower Trustworthiness'  => 'Trustworthy',
+            'Borrowers Age'             => 27,
+            'Loan length in Months'     => 12,
+            'Loan Amount'               => 90000
+        ]);
+
+        $surveyService = app()->make(SurveyService::class);
+        $surveyService->eveluateSurveyForm($payLoad, $survey);
+
+        return $request->all();
+    }
+}
